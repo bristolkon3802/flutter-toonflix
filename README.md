cc21db01f84e0f62e8b6d14e1dccd72e933b4ca1
bristolkon <bristolkon3802@gmail.com>
2024-02-05 17:28:19 +0900

    Hello World

---
+++ README.md
@@ -0,0 +1,11 @@
+# toonflix
+
+### 설치
+
+- flutter create toonflix
+- toonflix/code .
+
+### 추가 마켓플레이스
+
+- Dart
+- Flutter
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: const Color(0xFF181818),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 80, // 공백 생성
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end, // 오른쪽 정렬
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text(
+                          'Hey, Selena',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.8),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
