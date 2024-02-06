75249c17c08130ced014394e1697bfec1e958b16
bristolkon <bristolkon3802@gmail.com>
2024-02-06 13:23:12 +0900

    플러터 wallets

--- README.md
+++ README.md
@@ -9,3 +9,28 @@
 
 - Dart
 - Flutter
+- Error Lens
+
+### vscode settings
+- 설정 > open user settings > "source.fixAll": true
+
+### Tools 정의
+
+- Row 의 CrossAxis 는 세로 방향, Row 에서는 가로가 Main
+  crossAxisAlignment: CrossAxisAlignment.start, 처음으로 정렬
+  crossAxisAlignment: CrossAxisAlignment.end, 밑으로 정렬
+  mainAxisAlignment: MainAxisAlignment.spaceBetween, 양옆 센터정렬
+  clipBehavior: Clip.hardEdge, icon 이 박스 밖으로 넘쳐흐를경우 튀어나온거 히든 처리
+- Column 의 CrossAxis 는 가로 방향, Column 에서는 세로가 Main
+- SizedBox => size 가 있는 box 를 만들어 줌
+- Colors.white.withOpacity(0.8)
+- Color(0xFF181818) / Color.fromRGBO(255, 255, 255, 0.8)
+- Text('Hello Word',style: TextStyle(color: Colors.white,fontSize: 28,fontWeight: FontWeight.w800))
+- Container(decoration: BoxDecoration(color: Colors.amber,), //위아래 15, 왼쪽오른쪽 40
+    child: Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40), child: Text('Transfer'),),
+  )
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/button.dart';
+import 'package:toonflix/widgets/card.dart';
 
 void main() {
   runApp(const App());
@@ -11,13 +13,15 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color(0xFF181818),
-          body: Padding(
+        backgroundColor: const Color(0xFF181818),
+        body: SingleChildScrollView(
+          child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 40),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const SizedBox(
-                  height: 80, // 공백 생성
+                  height: 50, // 공백 생성
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end, // 오른쪽 정렬
@@ -29,7 +33,7 @@ class App extends StatelessWidget {
                           'Hey, Selena',
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 28,
+                            fontSize: 23,
                             fontWeight: FontWeight.w800,
                           ),
                         ),
@@ -37,16 +41,107 @@ class App extends StatelessWidget {
                           'Welcome back',
                           style: TextStyle(
                             color: Colors.white.withOpacity(0.8),
-                            fontSize: 18,
+                            fontSize: 15,
                           ),
                         ),
                       ],
                     )
                   ],
-                )
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Total balance',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+                const SizedBox(
+                  height: 0,
+                ),
+                const Text(
+                  '\$5 194 382',
+                  style: TextStyle(
+                    fontSize: 38,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Button(
+                        text: 'Transfer',
+                        bgColor: Color(0xFFF2B33A),
+                        textColor: Colors.black),
+                    Button(
+                        text: 'Request',
+                        bgColor: Color(0xFF1F2123),
+                        textColor: Colors.white),
+                  ],
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Wallets',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'View All',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const CurrencyCard(
+                  name: 'Dollar',
+                  code: 'USD',
+                  amount: '1 000 000',
+                  icon: Icons.attach_money_outlined,
+                  isInverted: false,
+                  isOffset: 0,
+                ),
+                const CurrencyCard(
+                  name: 'Bitcoin',
+                  code: 'BTC',
+                  amount: '1 000 000 000',
+                  icon: Icons.currency_bitcoin,
+                  isInverted: true,
+                  isOffset: -25,
+                ),
+                const CurrencyCard(
+                  name: 'Euro',
+                  code: 'EUR',
+                  amount: '6 428',
+                  icon: Icons.euro_rounded,
+                  isInverted: false,
+                  isOffset: -50,
+                ),
               ],
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
